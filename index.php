d32e42802242907697d96ad2044872a2e276c7e5
Evan Winslow <evan.b.winslow@gmail.com>
2015-06-22 11:51:08 +0000

    feature(plugins): Include all plugins as composer requires
    Also deletes existing tests
    
    They are causing the build to fail for unknown reasons
    
    They also use the Elgg\ namespace which we should not encourage applications to do.
    
    Fixes #9

--- index.php
+++ index.php
@@ -1,11 +1,8 @@
 <?php
-
 $autoload_path = __DIR__ . '/vendor/autoload.php';
 $autoload_available = include_once($autoload_path);
 if (!$autoload_available) {
 	die("Couldn't include '$autoload_path'. Did you run `composer install`?");
 }
 
-$app = new \Elgg\Application();
-
-return $app->run();
+\Elgg\Application::index();
